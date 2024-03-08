0af926a5dbe0a77c9401475a589c652874fbd311
garrettmichiels <michielsgarrett@gmail.com>
2024-03-08 15:04:57 -0500

    navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { moveActiveBar } from './navbar/nav.js';
 
 function App() {
+  document.addEventListener('DOMContentLoaded', moveActiveBar);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 style={{"fontFamily" : "RobotoCondensed-Black", "fontSize" : "150px"}}>Garrett Michiels</h1>
+  
       </header>
+    <body>
+      <nav className="navigation">
+            <ul className="nav-list">
+              <li className="active"><a onClick={moveActiveBar} href="#Home">Home</a></li>
+              <li className=""><a onClick={moveActiveBar} href="#education">Education</a></li>
+              <li className=""><a onClick={moveActiveBar} href="#experience">Experience</a></li>
+              <li className=""><a onClick={moveActiveBar} href="#projects">Projects</a></li>
+              <div className="active-bar"></div>
+            </ul>
+        </nav>
+        <script src="./navbar/nav.js"></script>
+    </body>
     </div>
   );
 }
---
+++ src/navbar/nav.js
@@ -0,0 +1,13 @@
+
+
+export function moveActiveBar(activeBar, item) {
+const list = document.querySelectorAll(".nav-list li");
+const nav = document.querySelector(".navigation");
+
+list.forEach((item) => {
+    item.addEventListener("click", function (e) {
+        list.forEach((li) => li.classList.remove("active"));
+        e.currentTarget.classList.add("active");
+    });
+});
+}
